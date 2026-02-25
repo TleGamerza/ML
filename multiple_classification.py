2284f84f5f2712cfd8fdf582a9d45a5d3ef701af
TleGamerza <1166105051575@mail.rmutt.ac.th>
2026-02-25 10:48:39 +0700

    Add files via upload

--- multiple_classification.py
+++ multiple_classification.py
@@ -16,10 +16,10 @@ stress_model = pickle.load(open("stress_model.sav",'rb'))
 
 
 with st.sidebar:
-    selected = option_menu('Classification',['LOAN','RidingMower','Stress'])
+    selected = option_menu('Classification',['LOAN','RidingMower','stress'])
 
     if(selected == 'Stress'):
-        st.title('Stress Prediction')
+        st.title('stress Prediction')
             #user input
         Age = st.text_input('Age')
         Gender = st.text_input('Gender')
@@ -33,7 +33,7 @@ with st.sidebar:
         Caffeine_Intake_Cups = st.text_input('Caffeine_Intake_Cups')
         Weekend_Screen_Time_Hours = st.text_input('Weekend_Screen_Time_Hours')
 
-        Stress_predict = ''
+        stress_predict = ''
     
     if st.button('Predict'):
         stress_predict = stress_model.predict([[
@@ -49,7 +49,7 @@ with st.sidebar:
             float(Caffeine_Intake_Cups),
             float(Weekend_Screen_Time_Hours)
         ]])     
-    st.success(Stress_predict)
+        st.success(stress_predict)
 
 
 if(selected == 'RidingMower'):
